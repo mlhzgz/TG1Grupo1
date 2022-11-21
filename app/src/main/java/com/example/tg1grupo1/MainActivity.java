f50d13c7012d5ae81d66c8191446bfb314210e35
Nicolás Del Amo <nico.delamo.perez@gmail.com>
2022-11-21 12:13:27 +0100

    Tema y Splash

--- app/src/main/java/com/example/tg1grupo1/MainActivity.java
+++ app/src/main/java/com/example/tg1grupo1/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Intent splash = new Intent(getApplicationContext(), splash.class);
         startActivity(splash);
-        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         creadorDeContenido();
